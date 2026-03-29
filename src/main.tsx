007c9571edab55f2dfb82e03333e5ee598a2c04c
[dyad] <carthuim@gmail.com>
2026-03-29 12:14:38 -0300

    [dyad] Adicionado "Wake-up Ping" precoce no main.tsx e explicadas as diferenças entre Realtime e estratégias de keep-alive do servidor. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// TRUQUE DE MESTRE: O "Wake-Up Ping" (Disparo de Acordar)
+// Isso dispara um mini-pedido para o banco de dados no exato milissegundo 
+// que o aplicativo abre, antes mesmo do React e das telas terminarem de carregar.
+// Assim, enquanto o usuário vê a interface instantaneamente (pelo cache local), 
+// o banco de dados (que pode estar dormindo) já está "acordando" em background.
+try {
+  fetch("https://vipigovrygzyjaibssra.supabase.co/rest/v1/profiles?select=id&limit=1", {
+    headers: {
+      "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpcGlnb3ZyeWd6eWphaWJzc3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0NzgzNTMsImV4cCI6MjA4NTA1NDM1M30.Z5hyETn-WMuagY6yiBlyFWTahUm7SSWl4j-m1uI4x9U",
+    }
+  }).catch(() => {});
+} catch (e) {}
+
 createRoot(document.getElementById("root")!).render(<App />);
